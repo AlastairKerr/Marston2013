001ccce5a3a97c1d70871804650012863fce66fe
Alastair Kerr <alastair.kerr@ed.ac.uk>
2013-06-17 10:36:49 +0100

    Workflows Part 2

--- workflow-all-cen.sh
+++ workflow-all-cen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+#Create tab delimited files from bedgraph files 
 for file in *bg 
 do
 ./Cen-boxplot.pl  $file
 done
-
+#R code to plot files 
 R --no-save < AllCen.r
--- workflow-each-cen.sh
+++ workflow-each-cen.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+#Create tab delimited files from bedgraph files 
 for file in *bg 
 do
 ./EveryCen-boxplot.pl $file
 done
-
+#R code for plotting 
 R --no-save < plot-everyboxplot.r

3142b4658af8e6249c0f40c0c5758aa19671085c
Alastair Kerr <alastair.kerr@ed.ac.uk>
2013-12-19 16:03:59 +0000

    Updates to work with samtools Version: 0.1.16 (r963:234) bwa Version: 0.6.1-r104, bedtools v2.16.2

--- workflow1.sh
+++ workflow1.sh
@@ -1,28 +1,46 @@
 #!/bin/bash
 
+# Updates to work with samtools Version: 0.1.16 (r963:234)
+# and bwa Version: 0.6.1-r104 
+# and bedtools v2.16.2
+
+
+
 #input is a fastq file '$1'
-export DB="sacCer3 location"
+export DB=/homes/genomes/s.cerevisiae/sacCer3/bwa_indexes/sacCer3
+
 #bwa to align 
-bwa aln $DB $1 -t 20 > $1.sai
+echo "## Aligning using bwa" 
+bwa aln $DB $1 -t 40 > $1.sai
 bwa samse $DB $1.sai $1  > $2.sam
+
 #sam to bam
-samtools -b -S $2.sam > $2.bam
+echo "## Converting sam to bam, and remove unmapped reads" 
+samtools view -h -b -S -F 4 $2.sam > $2.bam
+
 #sort 
-samtools sort $2.bam $2-all
+echo "## sorting bam"
+samtools sort -m 500000000000  $2.bam $2-all
+
 #remove 'dupicates'
-samtools rmdupse $2-all.bam $2-rmdup.bam
-#remove intermediate
+echo "## removing duplicates" 
+samtools rmdup -s $2-all.bam $2-rmdup.bam
+
+
+#remove intermediate  #tidy up
+echo "## remove intermediate & tidy up"
 rm $2.bam 
-#tidy up
 gzip -9 $2.sam & 
 
 #remove rDNA region
-bedtools intersect -v -a $2-rmdup.bam -b rDNA.bed  > $2-rDNA-rmdup.bam
-bedtools intersect -v -a $2-all.bam -b rDNA.bed  > $2-rDNA-all.bam
+echo "## intersecting"
+bedtools intersect -v -abam $2-rmdup.bam -b rDNA.bed  > $2-rDNA-rmdup.bam
+bedtools intersect -v -abam $2-all.bam -b rDNA.bed  > $2-rDNA-all.bam
 
 #Info for rDNA alone
 
 #this was ignored as region nr saturated with reads
+echo "## generating coverage bed"
 bedtools coverage -abam $2-rmdup.bam -b rDNA.bed -d  > $2-rmdup-rDNA.bed
 
 #This used in figures 
